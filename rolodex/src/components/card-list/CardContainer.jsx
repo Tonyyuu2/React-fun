712c9466ae078546c3b97657cfeccaa6dffea1c9
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-09-06 12:41:51 -0700

    add styling + refactor into separate components

---
+++ rolodex/src/components/card-list/CardContainer.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default class CardContainer extends Component {
+  render() {
+    const { name, email, id } = this.props;
+
+    return (
+      <div key={id} className="card-container">
+        <img
+          src={`https://robohash.org/${id}?set=set2&size=180x180`}
+          alt={`monster ${name}`}
+        />
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    );
+  }
+}
--- rolodex/src/components/card-list/CardList.jsx
+++ rolodex/src/components/card-list/CardList.jsx
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
+import "./card-list.styles.css";
+import CardContainer from "./CardContainer";
 
 export default class CardList extends Component {
   render() {
     const { monsters } = this.props;
 
     return (
-      <div>
-        {monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+      <div className="card-list">
+        {monsters.map((monster) => {
+          const { name, email, id } = monster;
+          return <CardContainer name={name} email={email} id={id} />;
+        })}
       </div>
     );
   }
